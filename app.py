e5441a720b454ef7e9b06a40237e438b52d7b7f6
Hong, Hyewon <hhong96@gatech.edu>
2022-11-22 18:06:49 -0500

    logic fixed

--- app.py
+++ app.py
@@ -18,109 +18,105 @@ def data_read(query):
       conn.close()
       return df
     
-    
-ls_en = data_read("select * from listings_enriched_final")
+def cbsa_input(user_cbsa):
+  family_type = data_read("select distinct family_type_dummy, case when family_type_dummy = 1 then 'Yes' else 'No' end as family_type_final from listings_enriched_final order by family_type_dummy desc")
+  cent = data_read("select distinct centrality_index_dummy from listings_enriched_final order by centrality_index_dummy")
+  yearbuilt = data_read("select * from call_yearbuilt order by yearbuilt_buckets_dummy desc")
+  bd = data_read("select distinct bedroom_bucket_dummy, cast(round(bedrooms_final) as char) as bedrooms_final from listings_enriched_final  group by bedroom_bucket_dummy, bedrooms_final order by bedroom_bucket_dummy")
+  sqft = data_read("select * from call_sqft")
+  hometype = data_read("select * from call_hometype")
+  income = data_read("select * from call_income")
+  price = data_read(f"select * from call_price where cbsa = {user_cbsa}")
+  age = data_read(f"select * from call_age where cbsa = {user_cbsa}")
+  return family_type, cent, yearbuilt, bd, sqft, hometype, income, price, age
 
 
-for i in range(len(ls_en.columns)):
-  if ls_en[ls_en.columns[i]].dtype == 'object' and ls_en.columns[i] != 'cbsatitle':
-    ls_en[ls_en.columns[i]] = ls_en[ls_en.columns[i]].str.title()
-
-label = {}
-for i in range(len(ls_en.columns)):
-    if (ls_en[ls_en.columns[i]].dtype == 'object') or ('bucket' in ls_en.columns[i]):
-      ls_en[ls_en.columns[i]].replace('_', ' ', regex=True, inplace=True)
-      ls_en[ls_en.columns[i]].replace('Population ', '', regex=True, inplace=True)
-      ls_en[ls_en.columns[i]].replace('Income ', '', regex=True, inplace=True)
-      ls_en[ls_en.columns[i]].replace('Between ', '', regex=True, inplace=True)
-      ls_en[ls_en.columns[i]].replace('And', '-', regex=True, inplace=True)
-      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
-    elif 'dummy' in ls_en.columns[i] and ('bucket' not in ls_en.columns[i]):
-      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
-    elif 'final' in ls_en.columns[i]:
-      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
 
 
-st.markdown("<div><h1 style='text-align: center;'>Where should you live?</h1></div>", unsafe_allow_html=True)
-
-
-output = []
-
-cbsa_param = st.selectbox("Select Geographic Area", options=sorted(label['cbsatitle']))
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-  home_type_param = st.selectbox("Home Type", options=sorted(label['homeType'])) 
-  income_param = st.selectbox("Income", options=sorted(label['majority_income_final']))
-  bedroom_param = st.select_slider("Num of Bedrooms", options=sorted(label['bedrooms_final']), on_change=None)
-
-with col2:
-  price_param = st.selectbox("Ideal Price Range", options=sorted(label['price_bucket']))
-  square_feet_param = st.selectbox("Size Preference", options=sorted(label['sqft_buckets_dummy']))
-  year_built_param = st.select_slider("Recently-Built Home", options=sorted(label['yearbuilt_buckets_dummy']), on_change=None)
-
-  
-with col3:
-  age_param = st.selectbox("Age", options=sorted(label['median_age_bucket']))
-  education_options_param = st.selectbox("Education level", options=sorted(label['majority_education_final']))
-  bathroom_param = st.select_slider("Num of Bathrooms", options=sorted(label['bathrooms_final']), on_change=None)
-
-with col4:
-  family_param = st.selectbox("Family-Oriented Neighborhood", options=sorted(label['family_type_dummy']))   
-  ind_param = st.selectbox("Occupation", options=sorted(label['majority_industry_final']))
-  centrality_param = st.select_slider("Closeness to Downtown", options=sorted(label['centrality_index_dummy']), on_change=None)  
-  
-
-if st.button('Predict'):
-  
-  query = """
-  select pred
-  from output_test
-  where 
-  cbsatitle = '{}'
-  and bedroom_bucket_dummy = replace(replace(replace(replace(replace(replace(cast('{}' as unsigned),2,1),3,2),4,2),5,2),6,3),7,3)
-  and sqft_buckets_dummy = cast('{}' as unsigned) 
-  and yearbuilt_buckets_dummy = cast('{}' as unsigned) 
-  and family_type_dummy = cast('{}' as unsigned) 
-  and centrality_index_dummy = cast('{}' as unsigned)
-  
-  and price_bucket = lower('{}')
-  and homeType = upper('{}')
-  and median_age_bucket =lower('{}') 
-  and majority_education_final = lower('{}') 
-  and majority_income_final = lower('{}')
-  """.format(cbsa_param, bedroom_param, 
-             square_feet_param, year_built_param, family_param, centrality_param,
-             price_param, home_type_param, age_param, education_options_param, 
-             income_param)
-  
-  # if data_read(query)['pred'].values[0] != None:
-  #   zipcode = data_read(query)['pred']
-  # else:
-  zipcode = 22314
-    
-  # %%
-  st.markdown("<div2><h2 style='text-align: center;'>We recommend you live in zipcode " + str(zipcode) + "!</h2><div2>", unsafe_allow_html=True)
-  
-  ls = data_read("select * from listing where zipcode = {} limit 10".format(zipcode))
-  ls['address'] = ls.apply(lambda x: '%s, %s, %s, %s' % (x['streetAddress'], x['city'], x['state'], x['zipcode']), axis=1)
-  ls['lat'] = 0
-  ls['lon'] = 0
-  
-  gmaps_key = googlemaps.Client(key=key.api_key)
-  
-  for i in range(len(ls)):
-    g = gmaps_key.geocode(ls['address'][i])
-    if g[0]["geometry"]["location"]["lat"] != 0:
-      ls['lat'][i] = g[0]["geometry"]["location"]["lat"]
-      ls['lon'][i] = g[0]["geometry"]["location"]["lng"]
-    
-  # %%
-  with st.container():
-    st.map(ls[['lat', 'lon']])
-    
 def main():
-    pass
+# %%    
+  cbsa = data_read("select distinct cbsa, cbsatitle from listings_enriched_final order by cbsa")
+
+  # %%
+
+  st.markdown("<div><h1 style='text-align: center;'>Where should you live?</h1></div>", unsafe_allow_html=True)
+  cbsa_param = st.selectbox("Select Geographic Area", options=cbsa['cbsatitle'].tolist())
+
+  if cbsa_param:
+    user_cbsa = cbsa[cbsa['cbsatitle'] == cbsa_param]['cbsa'].values[0]
+    user_cbsa = str(user_cbsa)
+    
+    family_type, cent, yearbuilt, bd, sqft, hometype, income, price, age = cbsa_input(user_cbsa)
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      home_type_param = st.selectbox("Home Type", options=hometype['hometype'].tolist()) 
+      income_param = st.selectbox("Income", options=income['majority_income_final'].tolist())
+      bedroom_param = st.select_slider("Num of Bedrooms", options=bd['bedrooms_final'].tolist(), on_change=None)
+
+    with col2:
+      price_param = st.selectbox("Ideal Price Range", price['price_bucket'].tolist())
+      square_feet_param = st.selectbox("Size Preference", sqft['sqft_final'].tolist())
+      year_built_param = st.selectbox("Home Year-Built", yearbuilt['year_built_final'].tolist())
+
+    with col3:
+      age_param = st.selectbox("Age", age['age_bucket'].tolist())
+      family_param = st.selectbox("Do you live with your family?", options=family_type['family_type_final'].tolist())   
+      centrality_param = st.select_slider("Do you prefer city?", options=cent['centrality_index_dummy'].tolist(), on_change=None)  
+      
+      
+    if st.button('Predict'):
+      home_type_param = hometype[hometype['hometype'] == home_type_param]['hometype_dummy'].values[0]
+      income_param = income[income['majority_income_final'] == income_param]['majority_income_dummy'].values[0]
+      bedroom_param = bd[bd['bedrooms_final'] == bedroom_param]['bedroom_bucket_dummy'].values[0]
+      age_param = age[age['age_bucket'] == age_param]['median_age_dummy'].values[0]
+      price_param = price[price['price_bucket'] == price_param]['price_dummy'].values[0]
+      square_feet_param = sqft[sqft['sqft_final'] == square_feet_param]['sqft_buckets_dummy'].values[0]
+      year_built_param  = yearbuilt[yearbuilt['year_built_final'] == year_built_param]['yearbuilt_buckets_dummy'].values[0]
+      family_param = family_type[family_type['family_type_final'] == family_param]['family_type_dummy'].values[0]
+      
+      
+      query = """
+      select pred
+      from output_test
+      where 
+      cbsa = '{}'
+      and hometype_dummy = '{}'
+      and majority_income_dummy = '{}'
+      and bedroom_bucket_dummy = '{}'
+      and median_age_dummy = '{}'
+      and price_dummy = '{}'
+      and sqft_buckets_dummy = '{}'
+      and yearbuilt_buckets_dummy = '{}'
+      and family_type_dummy = '{}'
+      and centrality_index_dummy = '{}'
+      """.format(user_cbsa, home_type_param, income_param, bedroom_param, age_param, price_param, square_feet_param, year_built_param, family_param, centrality_param)
+      
+      if data_read(query)['pred'].values[0] != None:
+        zipcode = data_read(query)['pred'].values[0]
+      else:
+        zipcode = 22314
+        
+      # %%
+      st.markdown("<div2><h2 style='text-align: center;'>We recommend you live in zipcode " + str(zipcode) + "!</h2><div2>", unsafe_allow_html=True)
+      
+      ls = data_read("select * from listing where zipcode = {} limit 10".format(zipcode))
+      ls['address'] = ls.apply(lambda x: '%s, %s, %s, %s' % (x['streetAddress'], x['city'], x['state'], x['zipcode']), axis=1)
+      ls['lat'] = 0
+      ls['lon'] = 0
+      
+      gmaps_key = googlemaps.Client(key=key.api_key)
+      
+      for i in range(len(ls)):
+        g = gmaps_key.geocode(ls['address'][i])
+        if g[0]["geometry"]["location"]["lat"] != 0:
+          ls['lat'][i] = g[0]["geometry"]["location"]["lat"]
+          ls['lon'][i] = g[0]["geometry"]["location"]["lng"]
+        
+      # %%
+      with st.container():
+        st.map(ls[['lat', 'lon']])
+    
   
 if __name__ == '__main__':
 	main()
