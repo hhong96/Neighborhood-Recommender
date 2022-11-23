c81365bc8b29219cda308613018f2050d3173534
Hong, Hyewon <hhong96@gatech.edu>
2022-11-23 13:56:06 -0500

    deploy test with api key uploaded

--- app.py
+++ app.py
@@ -19,19 +19,16 @@ def data_read(query):
       return df
     
 def cbsa_input(user_cbsa):
-  family_type = data_read("select distinct family_type_dummy, case when family_type_dummy = 1 then 'Yes' else 'No' end as family_type_final from listings_enriched_final order by family_type_dummy desc")
-  cent = data_read("select distinct centrality_index_dummy from listings_enriched_final order by centrality_index_dummy")
-  yearbuilt = data_read("select * from call_yearbuilt order by yearbuilt_buckets_dummy desc")
-  bd = data_read("select distinct bedroom_bucket_dummy, cast(round(bedrooms_final) as char) as bedrooms_final from listings_enriched_final  group by bedroom_bucket_dummy, bedrooms_final order by bedroom_bucket_dummy")
-  sqft = data_read("select * from call_sqft")
-  hometype = data_read("select * from call_hometype")
-  income = data_read("select * from call_income")
+  family_type = data_read(f"select distinct family_type_dummy, case when family_type_dummy = 1 then 'Yes' else 'No' end as family_type_final from listings_enriched_final where cbsa = {user_cbsa} order by family_type_dummy desc")
+  cent = data_read(f"select distinct centrality_index_dummy from listings_enriched_final where cbsa = {user_cbsa} order by centrality_index_dummy")
+  yearbuilt = data_read(f"select * from call_yearbuilt where cbsa = {user_cbsa} order by yearbuilt_buckets_dummy desc")
+  bd = data_read(f"select distinct bedroom_bucket_dummy, cast(round(bedrooms_final) as char) as bedrooms_final from listings_enriched_final where cbsa = {user_cbsa} order by bedroom_bucket_dummy")
+  sqft = data_read(f"select * from call_sqft where cbsa = {user_cbsa}")
+  hometype = data_read(f"select * from call_hometype where cbsa = {user_cbsa}")
+  income = data_read(f"select * from call_income where cbsa = {user_cbsa}")
   price = data_read(f"select * from call_price where cbsa = {user_cbsa}")
   age = data_read(f"select * from call_age where cbsa = {user_cbsa}")
   return family_type, cent, yearbuilt, bd, sqft, hometype, income, price, age
-
-
-
 
 def main():
 # %%    
@@ -62,7 +59,7 @@ def main():
     with col3:
       age_param = st.selectbox("Age", age['age_bucket'].tolist())
       family_param = st.selectbox("Do you live with your family?", options=family_type['family_type_final'].tolist())   
-      centrality_param = st.select_slider("Do you prefer city?", options=cent['centrality_index_dummy'].tolist(), on_change=None)  
+      centrality_param = st.selectbox("Do you prefer city?", options=cent['centrality_index_dummy'].tolist(), on_change=None)  
       
       
     if st.button('Predict'):
@@ -77,8 +74,8 @@ def main():
       
       
       query = """
-      select pred
-      from output_test
+      select zipcode
+      from output_vertical
       where 
       cbsa = '{}'
       and hometype_dummy = '{}'
@@ -91,14 +88,14 @@ def main():
       and family_type_dummy = '{}'
       and centrality_index_dummy = '{}'
       """.format(user_cbsa, home_type_param, income_param, bedroom_param, age_param, price_param, square_feet_param, year_built_param, family_param, centrality_param)
-      
-      # df = data_read(query)['pred']
-      # df
-      if data_read(query)['pred'].shape[0] == 0:
+
+
+      if data_read(query)['zipcode'].shape[0] == 0:
         st.markdown("<div2><h2 style='text-align: center;'> no data yet :( this is my zip code.</h2><div2>", unsafe_allow_html=True)
         zipcode = 22314
+        
       else:
-        zipcode = data_read(query)['pred'].values[0]
+        zipcode = data_read(query)['zipcode'].values[0]
         st.markdown("<div2><h2 style='text-align: center;'>We recommend you live in zipcode " + str(zipcode) + "!</h2><div2>", unsafe_allow_html=True)
         
       ls = data_read("select * from listing where zipcode = {} limit 10".format(zipcode))
---
+++ util/key.py
@@ -0,0 +1,6 @@
+host='cse6242.czj7hqwhnoml.us-east-1.rds.amazonaws.com'
+user="admin"
+password="cse6242110"
+port=3306
+database="realestate"
+api_key = "AIzaSyDqdnSfPkWIEFsuLtHcul9hyzm9B4-Q0-o"
