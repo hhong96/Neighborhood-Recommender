a062a71d96b7b7850115ba81b0ead810db2e59e5
Hong, Hyewon <hhong96@gatech.edu>
2022-11-24 22:18:13 -0500

    tab added + mapbox token

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from sqlalchemy import create_engine
 import googlemaps
 import util.key as key
 
+snippet = embed.embed_snippet(views=map)
+html = embed.html_template.format(title="", snippet=snippet)
+components.html(html, height=500,width=500)
+
+st.set_page_config(page_title="Neighborhood Recommender", page_icon=":house:", layout="wide", initial_sidebar_state="expanded")
 
 # Connection
 @st.cache
@@ -13,7 +18,7 @@ def get_cbsa():
       df = pd.read_sql(query, engine)
       return df
 
-@st.cache(allow_output_mutation=True)
+@st.cache
 def cbsa_input(user_cbsa):
   engine = create_engine(key.engine, echo=False)
   
@@ -56,6 +61,15 @@ def get_listing(zipcode):
   listing = pd.read_sql(query, engine)
   return listing
 
+
+@st.cache(allow_output_mutation=True)
+def get_county(zipcode):
+  engine = create_engine(key.engine, echo=False)
+  query = "select concat(county, ', ', city) as county from zip_county where zipcode = {}".format(zipcode)
+  county = pd.read_sql(query, engine)
+  return county
+
+
 cbsa = get_cbsa()
 
 
@@ -67,11 +81,13 @@ if 'other_input' not in st.session_state:
   
 if 'zipcode' not in st.session_state:
   st.session_state['zipcode'] = 0
-  
 
 
-st.markdown("<div><h1 style='text-align: center;'>Where should you live?</h1></div>", unsafe_allow_html=True)
-    
+st.markdown("# :house_buildings: Neighborhood Recommender")
+st.markdown("### CSE 6242 Proejct - Team 110")
+st.markdown("> Select an area you want to live in to get started, and fill out your preference and demographic information - We will find **the best neighbrohood for you**! :dancer:")    
+st.markdown("")
+
 with st.sidebar.form("other_form"):
     
   cbsa_param = st.selectbox("Select Geographic Area", options=[''] + cbsa['cbsatitle'].tolist())
@@ -110,17 +126,28 @@ with st.sidebar.form("other_form"):
         st.session_state['zipcode'] = zip[st.session_state['cbsa']].values[0]
       else:
         pass
-      
+
+
 if st.session_state['zipcode'] != 0:
-    st.markdown("<div2><h2 style='text-align: center;'>We recommend you live in zipcode " + str(st.session_state['zipcode']) + "!</h2><div2>", unsafe_allow_html=True)
-    
+    county = get_county(st.session_state['zipcode'])
+    with st.container():
+      print(county['county'].values[0])
+      st.markdown("## Your ideal neighborhood is " + str(county['county'].values[0]) + "! :tada:")
+      st.markdown(f"*Zip Code : {str(st.session_state['zipcode'])}*")
+
+
+tab1, tab2, tab3 = st.tabs(["listing", "yelp", "analysis"])   
+     
+with tab1: 
     ls = get_listing(st.session_state['zipcode'])
     ls['address'] = ls.apply(lambda x: '%s, %s, %s, %s' % (x['streetAddress'], x['city'], x['state'], x['zipcode']), axis=1)
     ls['lat'] = 0
     ls['lon'] = 0
 
     gmaps_key = googlemaps.Client(key=key.api_key)
-
+    df = ls[["streetAddress", "city", "state", "zipcode", "homeType_CD", "SqFt", "bedrooms", "bathrooms", "yearBuilt"]]
+    df.rename(columns={"streetAddress": "Street Address", "homeType_CD": "Home Type", "SqFt": "SqFt", "bedrooms": "# Bedroom", "bathrooms": "# Bathroom", "yearBuilt": "Year Built"}, inplace=True)
+    
     for i in range(len(ls)):
       g = gmaps_key.geocode(ls['address'][i])
       
@@ -129,7 +156,16 @@ if st.session_state['zipcode'] != 0:
         ls['lon'][i] = g[0]["geometry"]["location"]["lng"]
 
     with st.container():
+      st.markdown("### Here are some available houses in your ideal neighborhood!")
       st.map(ls[['lat', 'lon']])
+      st.table(df.style.format({"SqFt": "{:.0f}", "# Bedroom": "{:.0f}", "# Bathroom": "{:.0f}", "Year Built": "{:.0f}"}))
+      
+      
+with tab2:
+  pass
+
+with tab3:
+  pass
 
 def main():
   pass
